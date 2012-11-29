09a8d14440caf8988f64b40531dd1f36abcfb14d
S7012MY <l3asketballplayer@yahoo.com>
2012-11-29 16:50:01 +0200

    Update

--- aip/ana.cpp
+++ aip/ana.cpp
@@ -6,13 +6,33 @@ using namespace std;
 typedef pair<int,int> per;
 priority_queue<per,vector<per>,less<per> >h;
 int n,k;
+
+//<parsing>
+FILE* fin=fopen("ana.in","r");
+const unsigned maxb=800192;
+char buf[maxb];
+unsigned ptr=maxb;
+
+inline unsigned getInt(){
+    unsigned nr=0;
+	while(buf[ptr]<'0'||'9'<buf[ptr])
+		if(++ptr>=maxb)
+			fread(buf,maxb,1,fin),ptr=0;
+	while('0'<=buf[ptr]&&buf[ptr]<='9'){
+		nr=nr*10+buf[ptr]-'0';
+		if(++ptr>=maxb)
+			fread(buf,maxb,1,fin),ptr=0;
+	}
+	return nr;
+}
+//</parsing>
+
 int main () {
 	int i,el;
-	freopen("ana.in","r",stdin);
 	freopen("ana.out","w",stdout);
-	scanf("%d %d",&n,&k);
+	n=getInt(); k=getInt();
 	for(i=1;i<=n-1;++i){
-		scanf("%d",&el);
+		el=getInt();
 		if(i<=k)h.push(make_pair(el,i));
 		else if(h.top().first>el){
 			h.pop();
