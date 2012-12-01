321c40079ac12ddf62c1e96d37f4492d74188127
S7012MY <l3asketballplayer@yahoo.com>
2012-12-01 23:00:01 +0200

    Update

--- binar.cpp
+++ binar.cpp
@@ -3,22 +3,18 @@
 using namespace std;
 
 int n,m,ind[2005],v[2005];
-string mt[2005];
+char mt[2005][2005];
 
 void so(int l,int s,int d) {
-	cout<<l<<' '<<s<<' '<<d<<'\n';
 	if(l==n) return;
 	int a=s-1,b=d+1;
 	for(int i=s; i<=d; ++i)
 		if(mt[l][ind[i]]=='0') v[++a]=ind[i];
 		else v[--b]=ind[i];
 	for(int i=s; i<=a; ++i) ind[i]=v[i];
-	for(int i=d; i>a; --i) ind[a+d-i-1]=v[i];
-	cout<<a<<' '<<b<<'\n';
-	for(int i=s; i<=d; ++i) cout<<ind[i]<<' ';
-	cout<<'\n';
-	if(a>1) so(l+1,s,a);
-	if(a+1<d) so(l+1,a+1,d);
+	for(int i=a+1,j=d; i<=d; ++i,--j) ind[i]=v[j];
+	if(a>s) so(l+1,s,a);
+	if(b<d) so(l+1,a+1,d);
 }
 
 int main() {
@@ -26,7 +22,7 @@ int main() {
 	ofstream g("binar.out");
 	f>>n>>m;
 	f.get();
-	for(int i=0; i<n; ++i) getline(f,mt[i]);
+	for(int i=0; i<n; ++i) f>>mt[i];
 	for(int i=0; i<m; ++i) ind[i]=i;
 	so(0,0,m-1);
 	for(int i=0; i<m; ++i) g<<ind[i]+1<<' ';
