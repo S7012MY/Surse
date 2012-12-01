d17116aa417859262e2afd828147603951f27d15
S7012MY <l3asketballplayer@yahoo.com>
2012-12-01 22:50:01 +0200

    Update

--- binar.cpp
+++ binar.cpp
@@ -6,13 +6,17 @@ int n,m,ind[2005],v[2005];
 string mt[2005];
 
 void so(int l,int s,int d) {
+	cout<<l<<' '<<s<<' '<<d<<'\n';
 	if(l==n) return;
 	int a=s-1,b=d+1;
 	for(int i=s; i<=d; ++i)
 		if(mt[l][ind[i]]=='0') v[++a]=ind[i];
 		else v[--b]=ind[i];
-	for(int i=l; i<=a; ++i) ind[i]=v[i];
+	for(int i=s; i<=a; ++i) ind[i]=v[i];
 	for(int i=d; i>a; --i) ind[a+d-i-1]=v[i];
+	cout<<a<<' '<<b<<'\n';
+	for(int i=s; i<=d; ++i) cout<<ind[i]<<' ';
+	cout<<'\n';
 	if(a>1) so(l+1,s,a);
 	if(a+1<d) so(l+1,a+1,d);
 }
