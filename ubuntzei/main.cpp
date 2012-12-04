9b8281bdecfcfa971806e50eb2f834dd8a1af394
S7012MY <l3asketballplayer@yahoo.com>
2012-12-05 01:00:01 +0200

    Update

--- ubuntzei/main.cpp
+++ ubuntzei/main.cpp
@@ -13,16 +13,28 @@ using namespace std;
 typedef pair<int,int> per;
 typedef vector<per>::iterator it;
 
-int n,m,k,snod[DK],unde[DK],dmin[DK][DK];
+int n,m,k,snod[DK],unde[DK],dmin[DK][DK],dist[DN];
 vector<per> gr[DN];
 queue<int> c;
+bitset<DN> inq;
 
 void dm(int s) {
-    for(int i=1; i<=n; ++i) dmin[s][i]=(1<<30);
+    for(int i=1; i<=n; ++i) dist[i]=(1<<30);
+    dist[snod[s]]=0;
     for(c.push(snod[s]);c.size();c.pop()) {
         int fr=c.front();
-        for
+        inq[fr]=0;
+        for(it i=gr[fr].begin(); i!=gr[fr].end(); ++i) if(dist[i->x]>dist[s]+i->y) {
+            dist[i->x]=dist[s]+i->y;
+            if(!inq[i->x]) {
+                c.push(i->x);
+                inq[i->x]=1;
+            }
+        }
     }
+    for(int i=1; i<=n; ++i) cout<<dist[i]<<' ';
+    cout<<'\n';
+    for(int i=1; i<=n; ++i) if(unde[i]) dmin[s][unde[i]]=dist[i];
 }
 
 int main()
@@ -30,17 +42,22 @@ int main()
     ifstream f("ubuntzei.in");
     ofstream g("ubuntzei.out");
     f>>n>>m>>k;
-    for(int i=1; i<=k; ++i) {
+    ++k;
+    for(int i=2; i<=k; ++i) {
         f>>snod[i];
         unde[snod[i]]=i;
     }
-    snod[0]=1; snod[++k]=n;unde[n]=k;
+    snod[1]=unde[1]=1;unde[1]=1; snod[++k]=n;unde[n]=k;
     for(int i=1; i<=m; ++i) {
         int a,b,c;
         f>>a>>b>>c;
         gr[a].push_back(mp(b,c));
         gr[b].push_back(mp(a,c));
     }
-    for(int i=0; i<=k; ++i) dm(i);
+    for(int i=1; i<=k; ++i) dm(i);
+    /*for(int i=1; i<=k; ++i) {
+        for(int j=1; j<=k; ++j) cout<<dmin[i][j]<<' ';
+        cout<<'\n';
+    }*/
     return 0;
 }
