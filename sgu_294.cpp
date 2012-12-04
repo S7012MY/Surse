2904a20016ebe13480bf6284204964998fe59b37
S7012MY <l3asketballplayer@yahoo.com>
2012-12-04 13:40:01 +0200

    Update

---
+++ sgu_294.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#define DN 200005
+using namespace std;
+
+int n,r=0;
+
+int gcd(int a,int b) {
+	for(int c;b;c=a%b,a=b,b=c);
+	return a;
+}
+
+int main() {
+	freopen("asd.in","r",stdin);
+	cin>>n;
+	printf("bleg");
+	for(int i=1; i<=n; ++i) r+=(1<<gcd(i,n));
+	cout<<r;
+	return 0;
+}
