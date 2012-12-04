9944f0b3feea68cf27a46d71e1b31c5bf49b2b8a
S7012MY <l3asketballplayer@yahoo.com>
2012-12-04 23:40:01 +0200

    Update

--- matrice2/main.cpp
+++ matrice2/main.cpp
@@ -3,13 +3,19 @@
 #define DN 305
 using namespace std;
 
-int n,q,m[DN][DN],pre[DN*DN];
+int n,q,m[DN][DN],pre[DN*DN],l;
+int dx[]={1,0,-1,0},dy[]={0,1,0,-1};
 
 int main()
 {
     ifstream f("matrice2.in");
     ofstream g("matrice2.out");
-    f>>n>>q;
+    f>>n>>q; l=n*n;
+    for(int i=1; i<=n; ++i) for(int j=1; j<=n; ++j) f>>m[i][j];
 
+    for(int s=(1<<20);s;s>>=1) {
+        for(int i=1; i<=lm; ++i) pre[i]=i;
+        for(int i=1; i<=n; ++i) for(int j=1; j<=n; ++j) if(m[i][j])
+    }
     return 0;
 }
