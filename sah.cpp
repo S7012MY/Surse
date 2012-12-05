f03373572f9bcd7a54c194c7a19592f83df86a7b
S7012MY <l3asketballplayer@yahoo.com>
2012-12-05 18:40:01 +0200

    Update

--- sah.cpp
+++ sah.cpp
@@ -5,9 +5,9 @@ int n,m;
 
 int main() {
 	ifstream f("sah.in");
-	ofstream g("sah.out");
 	f>>n;
 	m=n-1;
+	ofstream g("sah.out");
 	for(int i=1; i<m; i+=2) {
 		int lc=i,p=(i+1)/2;
 		for(int j=1; j<=p; ++j) g<<lc<<' ';
@@ -20,7 +20,6 @@ int main() {
 		for(int j=p+1; j<=n; ++j) g<<lc<<' ';
 		g<<'\n';
 	}
-	n;
 	for(int i=1; i<3; ++i) {
 		for(int j=1; j<=n; ++j) g<<m<<' ';
 		g<<'\n';
