d9cd055ff7a5a99ba48f2ff51ce09b29f6c1e482
S7012MY <l3asketballplayer@yahoo.com>
2012-12-06 18:00:01 +0200

    Update

---
+++ cf153b/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define DN 105
+using namespace std;
+
+int n,k,p[DN],q[DN],f[DN],np[DN];
+
+int main()
+{
+    cin>>n>>k;
+    for(int i=1; i<=n; ++i) {
+        cin>>q[i];
+        p[i]=i;
+    }
+    for(;k;) {
+        for(int i=1; i<=n; ++i) np[i]=p[q[i]];
+        for(int i=1; i<=n; ++i) p[i]=np[i];
+        --k;
+        int ok=1;
+        for(int i=1; i<=n; ++i) if(p[i]!=q[i]) ok=0;
+        if(ok && (k%2)==0) {
+            cout<<"YES";
+            return 0;
+        }
+    }
+    cout<<"NO";
+    return 0;
+}
