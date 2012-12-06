8cb0b5a5721e28e8e14757070c975767c750d6fa
S7012MY <l3asketballplayer@yahoo.com>
2012-12-06 11:00:01 +0200

    Update

--- oras.cpp
+++ oras.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int n;
 char m[205][205];
+const char impar[][]= {
+	"010",
+	"001",
+	"100"
+},par[][]={
+	"011100",
+	"001110",
+	"000101",
+	"000011",
+	"110000"
+};
+
 
 int main() {
 	ifstream f("oras.in");
@@ -13,3 +25,5 @@ int main() {
 		return 0;
 	}
 	for(int i=0; i<n; ++i) for(int j=0; j<n; ++j) m[i][j]='0';
+	if(n&1) {
+		
