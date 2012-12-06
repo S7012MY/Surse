9eba1a2154964a08401c812dca0627f94c693f0b
S7012MY <l3asketballplayer@yahoo.com>
2012-12-06 11:50:01 +0200

    Update

--- sir23.cpp
+++ sir23.cpp
@@ -1,21 +1,24 @@
 #include <fstream>
+#include <iostream>
 #define DN 1005
 using namespace std;
 
-int n,nr[DN][DN];
+int n,nr[DN][DN],r;
 
 /*
  *  nr[i][i]=1
- *  nr[i][lst]=nr[i-3][j]
+ *  nr[i][lst]=(lst-j)*nr[i-3][j]
 */
+
+
 
 int main() {
 	ifstream f("sir23.in");
 	ofstream g("sir23.out");
 	f>>n;
 	for(int i=1; i<=n; ++i) nr[1][i]=nr[2][i]=1;
-	for(int i=4; i<=n; ++i) for(int lst=1; lst<=n; ++lst) {
-		for(int j=1; j<=lst; ++j) for(int k=1; k<=lst; ++k) nr[i][lst]=nr[i-2][j]*nr[i-3];
-	}
-	for(int i=1; i<=n; ++i)
+	for(int i=1; i<=n; ++i) for(int j=1; j<i; ++j) nr[3][i]+=nr[1][j];
+	for(int i=1; i<=n; ++i) r+=nr[n][i];
+	g<<r;
+	return 0;
 }
