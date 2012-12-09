8394ee9a2457a06e7078998481dbe6e690751524
S7012MY <l3asketballplayer@yahoo.com>
2012-12-09 13:50:01 +0200

    Update

--- photo/main.cpp
+++ photo/main.cpp
@@ -15,14 +15,14 @@ per p[DN];
 int memo(int ls, int ld, int ih) {
     if(bst[ls][ld][ih]!=-1) return bst[ls][ld][ih];
     vector<int> v;
-    for(int i=ls; i<=ld; ++i) if(p[i].y>p[ih].y) v.push_back(i);
+    for(int i=ls; i<=ld; ++i) if(p[i].y>=p[ih].y) v.push_back(i);
     if(v.size()<2) {
         bst[ls][ld][ih]=v.size();
         return v.size();
     }
     int r=DN,sz=v.size();
     for(int i=0; i<sz-1; ++i) r=min(r,memo(ls,v[i],ih)+memo(v[i+1],ld,ih));
-    int hmax=DN,hmin=DN,ind,nr,cnt;
+    int hmax=DN,hmin=DN,ind=-1,cnt=0;
     if(p[v[sz-1]].x-p[v[0]].x) hmax=a/p[v[sz-1]].x-p[v[0]].x;
     for(int i=0; i<sz; ++i) if(p[v[i]].y>hmax){
         ++cnt;
@@ -31,8 +31,8 @@ int memo(int ls, int ld, int ih) {
             ind=v[i];
         }
     }
-    if(cnt!=v.size()) r=min(r,1+memo(ls,ld,ind));
-    if(hmax==DN) r=1;
+    if(cnt!=v.size() && ind!=-1) r=min(r,1+memo(ls,ld,ind));
+    if(ind==-1) r=min(r,1);
     bst[ls][ld][ih]=r;
     return r;
 }
@@ -44,7 +44,7 @@ int main()
     f>>n>>a;
     for(int i=1; i<=n; ++i) f>>p[i].x>>p[i].y;
     sort(p+1,p+n+1);
-    for(int i=1; i<=n; ++i) for(int j=1; j<=n; ++j) for(int k=0; k<=n; ++k) bst[i][j][k]=-1;
+    for(int i=0; i<=n; ++i) for(int j=0; j<=n; ++j) for(int k=0; k<=n; ++k) bst[i][j][k]=-1;
     g<<memo(1,n,0);
     return 0;
 }
