3c267b4835fb463c381443dc3203b32d2e831023
S7012MY <l3asketballplayer@yahoo.com>
2012-12-11 22:50:01 +0200

    Update

--- coins/main.cpp
+++ coins/main.cpp
@@ -5,7 +5,8 @@
 #define DN 22
 using namespace std;
 
-int bst[1<<DN],n,rez,nrb[1<<DN],s[DN];
+char bst[1<<DN];
+int n,rez,s[DN];
 
 int count(int nr) {
     if(nr==0) return 0;
@@ -18,24 +19,20 @@ int count(int nr) {
 
 
 int memo(int state) {
-    int r=(1<<30);
     if(bst[state]!=-1) return bst[state];
-    int lb=-1;
+    int lb=-1,r=1;
     for(int i=0; i<22; ++i)
         if(state&(1<<i)) {
             int ns=state;
             if(lb!=-1) {
                 ns^=(1<<i);
                 ns|=(1<<lb);
-                r=min(r,memo(ns));
+                r&=memo(ns);
             }
         }else lb=i;
-    if(r==(1<<30) || !r) {
-        bst[state]=nrb[state];
-        return nrb[state];
-    }
-    bst[state]=0;
-    return 0;
+    r^=1;
+    bst[state]=r;
+    return r;
 }
 
 //<parsing>
@@ -61,17 +58,20 @@ inline unsigned getInt(){
 int main()
 {
     ofstream g("coins.out");
-    for(int i=0; i<(1<<DN); ++i) bst[i]=-1,nrb[i]=count(i);
+    for(int i=0; i<(1<<DN); ++i) bst[i]=-1;
     for(n=getInt();n;--n) {
-        int si=0,x;
+        int si=0,x,cnt=0;
         for(int i=0; i<22; ++i) s[i]=getInt();
         reverse(s,s+22);
         for(int i=0; i<22; ++i) {
             si<<=1;
-            if(s[i])si|=1;
+            if(s[i]) {
+                si|=1;
+                ++cnt;
+            }
         }
        // cout<<memo(si)<<'\n';
-        rez+=memo(si);
+        if(memo(si)) rez+=cnt;
     }
     g<<rez;
     return 0;
