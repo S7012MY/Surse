ac0657dc80e4cb09ab43eb8ddb0ddb1f536c577e
S7012MY <l3asketballplayer@yahoo.com>
2012-12-11 21:10:01 +0200

    Update

--- coins/main.cpp
+++ coins/main.cpp
@@ -3,20 +3,36 @@
 #define DN 22
 using namespace std;
 
-int bst[1<<DN],n,rez;
+int bst[1<<DN],n,rez,nrb[1<<DN];
+
+int count(int nr) {
+    if(nr==0) return 0;
+    int r=0;
+    do {
+        ++r;
+    }while(nr &= nr-1);
+    return r;
+}
+
 
 int memo(int state) {
-    int r=(1<<30)
+    int r=(1<<30);
     if(bst[state]!=-1) return bst[state];
-    int lb=-1
+    int lb=-1;
     for(int i=0; i<22; ++i)
         if(state&(1<<i)) {
             int ns=state;
             if(lb!=-1) {
                 ns^=(1<<i);
                 ns|=(1<<lb);
+                r=min(r,memo(ns));
             }
         }else lb=i;
+    if(r==(1<<30) || !r) {
+        bst[state]=nrb[state];
+        return nrb[state];
+    }
+    bst[state]=0;
 }
 
 
@@ -24,7 +40,7 @@ int main()
 {
     ifstream f("coins.in");
     ofstream g("coins.out");
-    for(int i=1; i<(1<<DN); ++i) bst[i]=-1;
+    for(int i=0; i<(1<<DN); ++i) bst[i]=-1,nrb[i]=count(i);
     for(f>>n;n;--n) {
         int si=0,x;
         for(int i=0; i<22; ++i) {
