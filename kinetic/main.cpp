2ba5ac21f73e12298a50ffbdbb83a483a2984726
S7012MY <l3asketballplayer@yahoo.com>
2012-12-16 10:30:01 +0200

    Update

--- kinetic/main.cpp
+++ kinetic/main.cpp
@@ -4,21 +4,27 @@
 #include <vector>
 #define DN 505
 #define DT 10005
+#define CNST 10002
 using namespace std;
 
 int n,m;
-vector<int> poz[DN*DT];
+vector<int> poz[DT];
+
+int getvl(int vl, int t) {
+  int a=vl%10000,b=vl/10000;
+  return a+b*t;
+}
 
 int lb(int u,int vl) {
   int ls=0,ld=poz[u].size()-1,m;
   for(;ls<=ld;) {
     m=(ls+ld)>>1;
-    if(poz[u][m]<vl) ls=m+1;
+    if(poz[u][m]<getvl(vl,u)) ls=m+1;
     else ld=m-1;
   }
-  for(;poz[u][ls]>=vl && ls; --ls);
-  for(;poz[u][ls]<vl && ls<poz[u].size()-1;++ls);
-  if(poz[u][ls]<vl) return -1;
+  for(;poz[u][ls]>=getvl(vl,u) && ls; --ls);
+  for(;poz[u][ls]<getvl(vl,u) && ls<poz[u].size()-1;++ls);
+  if(poz[u][ls]<getvl(vl,u)) return -1;
   return ls;
 }
 
@@ -26,11 +32,11 @@ int ub(int u,int vl) {
   int ls=0,ld=poz[u].size()-1,m;
   for(;ls<=ld;) {
     m=(ls+ld)>>1;
-    if(poz[u][m]<vl) ls=m+1;
+    if(poz[u][m]<getvl(vl,u)) ls=m+1;
     else ld=m-1;
   }
-  for(;poz[u][ls]>vl && ls>=0; --ls);
-  for(;poz[u][ls]<=vl && ls<poz[u].size();++ls);
+  for(;poz[u][ls]>getvl(vl,u) && ls>=0; --ls);
+  for(;poz[u][ls]<=getvl(vl,u) && ls<poz[u].size();++ls);
   return ls;
 }
 
@@ -42,16 +48,17 @@ int main()
     for(int i=0; i<n; ++i) {
       int a,b;
       f>>a>>b;
-      for(int t=0; t<10001; ++t) poz[t].push_back(a+b*t);
+      for(int t=0; t<CNST; ++t) poz[t].push_back(a+b*t);
+      poz[CNST].push_back(b*10000+a);
     }
-    for(int t=0; t<10001; ++t) sort(poz[t].begin(),poz[t].end());
+    for(int t=0; t<=CNST; ++t) sort(poz[t].begin(),poz[t].end());
     for(int i=0; i<m; ++i) {
       int x,y,t;
       f>>x>>y>>t;
       if(x>y) swap(x,y);
       int st,dr;
       st=lb(t,x); dr=ub(t,y);
-      cout<<t<<' '<<x<<' '<<st<<' '<<y<<' '<<dr<<'\n';
+     // cout<<t<<' '<<x<<' '<<st<<' '<<y<<' '<<dr<<'\n';
       if(st==-1) {
         g<<"0\n";
         continue;
