53837f9066d321e1831d6808512751528166f5f2
S7012MY <l3asketballplayer@yahoo.com>
2012-12-18 19:30:01 +0200

    Update

--- taie/main.cpp
+++ taie/main.cpp
@@ -7,13 +7,36 @@ using namespace std;
 
 typedef pair<double,double> per;
 pair<per,per> seg[DN];
-int n;
+per p[DN*2];
+int n,sz;
+
+double id(per a, per b, per c) {
+  double p=a.x*b.y+b.x*c.y+c.x*a.y,m=-b.x*a.y-c.x*b.y-a.x*c.y;
+  return p-m;
+}
 
 int main()
 {
     ifstream f("taie.in");
     ofstream g("taie.out");
     f>>n;
-    for(int i=1; i<=n; ++i) f>>seg[i].x.x>>seg[i].x.y>>seg[i].y.x>>seg[i].y.y;
+    for(int i=1; i<=n; ++i) {
+      f>>seg[i].x.x>>seg[i].x.y>>seg[i].y.x>>seg[i].y.y;
+      p[++sz]=seg[i].x; p[++sz]=seg[i].y;
+    }
+    cout<<id(make_pair(0,0),make_pair(1,1),make_pair(1,0));
+    for(int i=1; i<=sz; ++i)
+      for(int j=i+1; j<=sz; ++j) {
+        int ok=1;
+        for(int k=1; k<=n; ++k) if(!((id(p[i],p[j],seg[k].x)<=0 && id(p[i],p[j],seg[k].y)>=0) ||(id(p[i],p[j],seg[k].x)>=0 && id(p[i],p[j],seg[k].y)<=0) )) {
+          ok=0;
+          break;
+        }
+        if(ok) {
+          g<<p[i].x<<' '<<p[i].y<<' '<<p[j].x<<' '<<p[j].y<<'\n';
+          return 0;
+        }
+      }
+    g<<-1;
     return 0;
 }
