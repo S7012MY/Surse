6e6e039c1e015bd13f1671b2a9465a2b1fc585da
S7012MY <l3asketballplayer@yahoo.com>
2012-12-18 18:50:02 +0200

    Update

--- taie/main.cpp
+++ taie/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
+#include <fstream>
+#define DN 1005
+#define x first
+#define y second
 using namespace std;
+
+typedef pair<double,double> per;
+pair<per,per> seg[DN];
+int n;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    ifstream f("taie.in");
+    ofstream g("taie.out");
+    f>>n;
+    for(int i=1; i<=n; ++i) f>>seg[i].x.x>>seg[i].x.y>>seg[i].y.x>>seg[i].y.y;
     return 0;
 }
