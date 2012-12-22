243499220c8b02de9c599fbc8f3dda8464c58160
S7012MY <l3asketballplayer@yahoo.com>
2012-12-22 18:40:01 +0200

    Update

--- cf157E/main.cpp
+++ cf157E/main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <bitset>
 #define DN 100005
 using namespace std;
 
 typedef vector<int>::iterator it;
 
-int n,pre[DN];
+bitset<DN> viz;
+int n,m,fs[DN*2],lst[DN*2],rez[DN],ai[4*DN],nr[4*DN];
+vector<int> gr[DN];
+
+void dfs(int s) {
+    viz[s]=1;
+    fs[s]=++sz;
+    for(it i=gr[s].begin(); i!=gr[s].end(); ++i) if(!viz[*i]) dfs(*i);
+    lst[s]=sz;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cin>>n>>m;
+    for(int i=1; i<n; ++i) {
+      int a,b;
+      cin>>a>>b;
+      gr[a]
+    }
     return 0;
 }
