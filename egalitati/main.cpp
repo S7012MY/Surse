f683a2a2e70e2a7af8d2fe8dcc257066e57eb88d
S7012MY <l3asketballplayer@yahoo.com>
2012-12-27 11:10:01 +0200

    Update

--- egalitati/main.cpp
+++ egalitati/main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
-int n;
+string s;
+int nrd;
 
 int main()
 {
     ifstream f("egalitati.in");
     ofstream g("egalitati.out");
-    f>>n;
+    getline(f,s);
+    s+=';';
+    int rez=1;
+    for(int i=0; i<s.size(); ++i) {
+      if(s[i]=='(') ++nrd;
+      else if(s[i]==')') {
+        if(nrd<1) rez=0;
+        else --nrd;
+      }else if(s[i]=='=') {
+        if(nrd>0) rez=0;
+        nrd=0;
+      }else if(s[i]==';') {
+        g<<rez<<'\n';
+        nrd=0;
+      }
+    }
     return 0;
 }
