c144060c8a5888bbae7db07d4a37cc4569c7405e
S7012MY <l3asketballplayer@yahoo.com>
2012-12-30 13:50:01 +0200

    Update

--- x/main.cpp
+++ x/main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 string a,b;
 
-int n,m,q
+int n,m,q;
+vector<int> z,zr;
+
+void pp() {
+  string s;
+  s=a+'$'+b;
+}
 
 int main()
 {
     ifstream f("x.in");
     ofstream g("x.out");
+    f>>n>>m>>q;
+    f>>a>>b;
+    pp();
     return 0;
 }
