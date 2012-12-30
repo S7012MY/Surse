1981418b868e73a014000de389dd11fed50012b9
S7012MY <l3asketballplayer@yahoo.com>
2012-12-30 14:00:01 +0200

    Update

--- x/main.cpp
+++ x/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 string a,b;
@@ -9,9 +10,37 @@ string a,b;
 int n,m,q;
 vector<int> z,zr;
 
+void faZ(string s,vector<int> &z) {
+  z.push_back(0);
+  int n=s.size();
+  int l=0,r=0,k;
+  for(int i=1; i<n; ++i)
+    if(i>r) {
+      l=r=i;
+      for(;r<n && s[r-l]==s[r];++r);
+      z.push_back(r-l); --r;
+    }else {
+      k=i-l;
+      if(z[k]<r-i+1) z.push_back(z[k]);
+      else {
+        l=i;
+        for(;r<n && s[r-l]==s[r];++r);
+        z.push_back(r-l); --r;
+      }
+    }
+}
+
 void pp() {
-  string s;
-  s=a+'$'+b;
+  string s,ra=a,rb=b;
+  reverse(ra.begin(),ra.end());
+  reverse(rb.begin(),rb.end());
+  s=b+'$'+a;
+  faZ(s,z);
+  cout<<s<<'\n';
+  for(int i=0; i<z.size(); ++i) cout<<z[i];
+  cout.flush();
+  s=rb+'$'+ra;
+  faZ(s,zr);
 }
 
 int main()
@@ -21,5 +50,10 @@ int main()
     f>>n>>m>>q;
     f>>a>>b;
     pp();
+    for(;q;--q) {
+      int l,r;
+      f>>l>>r;
+      g<<min(z[r+m],99999)<<'\n';
+    }
     return 0;
 }
