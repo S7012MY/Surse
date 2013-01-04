9fdb894a26607573349429b545de59dd835d1e5a
S7012MY <l3asketballplayer@yahoo.com>
2013-01-04 14:40:01 +0200

    Update

--- acm_subregional_2012/F.cpp
+++ acm_subregional_2012/F.cpp
@@ -23,6 +23,7 @@ cmb[0][0]=1;
       for(int i=0;i<20; ++i) if(pd[i]+lst<a) lst+=pd[i],l2=pd[i];
       a-=lst;
       l2<<=1;
+      cout<<a<<' '<<l2<<'\n';
       cout<<(cmb[l2/2][(a-1)/2]*1LL*cmb[l2/2][(a-1)/2]*(l2-a+1))%MOD<<'\n';
     }
   }
