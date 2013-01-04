974d0bba8dcbb059609afd1c5ef8e919bc64de68
S7012MY <l3asketballplayer@yahoo.com>
2013-01-04 14:20:01 +0200

    Update

--- acm_subregional_2012/G.cpp
+++ acm_subregional_2012/G.cpp
@@ -5,14 +5,15 @@ using namespace std;
 
 int main() {
 //freopen("inp.in","r",stdin);
-  int a,b,c,d;
+  double a,b,c,d;
   for(;!feof(stdin);) {
-    scanf("%d %d %d %d\n",&a,&b,&c,&d);
-    int r1=0,r2=0;
+    scanf("%lf%lf%lf%lf\n",&a,&b,&c,&d);
+    int r1=0,r2=0,cc=c,cd=d;
     if(a>b) swap(a,b);
     if(c>d) swap(c,d);
-    for(;c>a;c/=2,++r1);
-    for(;d>b;d/=2,++r1);
-    cout<<r1<<'\n';
+    for(;c>a+1e-8;c/=2,++r1);
+    for(;d>b+1e-8;d/=2,++r1);
+
+    cout<<min(r1,r2)<<'\n';
   }
 }
