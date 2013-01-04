520bbe2d4b4070590c99dd1daaa1741dd5536e16
S7012MY <l3asketballplayer@yahoo.com>
2013-01-04 16:30:01 +0200

    Update

--- acm_subregional_2012/b.cpp
+++ acm_subregional_2012/b.cpp
@@ -1,1 +1,15 @@
+/*#include <iostream>
+#include <string>
+using namespace std;
 
+int nr[20][20];
+
+int main() {
+  int t;
+  nr[1][1]=1;
+  for(int i=2; i<=20; ++i) for(int j=1; j<=20; ++j) nr[i][j]=nr[i-1][j-1]*i+nr[i-1][j]*j;
+  cout<<nr[3][2];
+  for(cin>>t;t;--t) {
+  }
+}
+*/
