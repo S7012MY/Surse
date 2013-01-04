9c7ef3b69bfe3674a5c6f5d7aaa3f34ee02550ca
S7012MY <l3asketballplayer@yahoo.com>
2013-01-04 15:30:01 +0200

    Update

--- acm_subregional_2012/I.cpp
+++ acm_subregional_2012/I.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#define x first
+#define y second
+using namespace std;
 
+int t,n,m;
+vector<int> v;
+map<int,int> mp;
+
+int main() {
+  for(cin>>t;t;--t) {
+    v.clear(); mp.clear();
+    cin>>n>>m;
+  }
+}
